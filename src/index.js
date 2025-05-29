ea4b8e85060a78f98be2bbcc549c8238da93b643
Mscode04 <ms.shaheenkp@gmail.com>
2025-05-29 20:10:47 +0530

    updates-05-10

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { register } from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
